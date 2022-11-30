b387bc17f7fa1d684fa5e7b7da4db731306ef409
LeeJunHui00 <62248164+LeeJunHui00@users.noreply.github.com>
2022-11-30 19:39:00 +0900

    .

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyPaser = require("body-parser");
+
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyPaser.urlencoded({ extended: true }));
+
+//application/json
+app.use(bodyPaser.json());
 
 const mongoose = require("mongoose");
 mongoose
-    .connect(
-        "mongodb+srv://JunHui:2022@cluster0.baul5n7.mongodb.net/?retryWrites=true&w=majority",
-        {}
-    )
+    .connect(config.mongoURI, {})
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World! auto restart");
 });
+
+app.post("/register", (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    });
+});
+// 회원 가입 위해 필요한 정보들을 client 에서 가져오면
+// 그것들을 데이터 베이스에 넣어준다.
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- models/User.js
+++ models/User.js
@@ -11,6 +11,7 @@ const userSchema = mongoose.Schema({
         unique: 1,
     },
     password: {
+        type: String,
         minlength: 5,
     },
     lastname: {
